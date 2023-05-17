9aa25a7214bcb0ff8de7a40599a826bf2d1d8bd9
Fischer, Angelina <angelina.fischer@sva.de>
2023-05-17 17:13:14 +0200

    Initialize categories and set default category

--- auctions/models.py
+++ auctions/models.py
@@ -30,11 +30,27 @@ class User(AbstractUser):
         return self.username
 
 
+class CategoryManager(models.Manager):
+    def create_category(self, name):
+        return self.create(name=name)
+    
+    def get_default_category(self):
+        return self.get(name=dict(CATEGORY_CHOICES)[NONE])
+
+
 class Category(models.Model):
-    name = models.CharField(max_length=4, choices=CATEGORY_CHOICES, default=NONE)
+    name = models.CharField(max_length=64)
+
+    objects = CategoryManager()
 
     def __str__(self):
         return self.name
+    
+
+# Initialize all categories
+for _, name in CATEGORY_CHOICES:
+    if not Category.objects.filter(name=name).exists():
+        Category.objects.create_category(name)
 
 
 class Listing(models.Model):
@@ -42,8 +58,8 @@ class Listing(models.Model):
     description = models.TextField(max_length=255)
     starting_bid = models.DecimalField(max_digits=9, decimal_places=2, default=0.00)
     current_bid = models.DecimalField(max_digits=9, decimal_places=2, default=0.00)
-    image_url = models.URLField(max_length=255, blank=True, default="https://t3.ftcdn.net/jpg/04/34/72/82/240_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category_listings", blank=True)
+    image_url = models.URLField(max_length=255, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category_listings", blank=True, default=Category.objects.get_default_category)
     seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="seller_listings")
     closed = models.BooleanField(default=False)
     creation_date = models.DateTimeField(auto_now_add=True)
